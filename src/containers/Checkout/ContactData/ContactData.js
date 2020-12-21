fe8c28f0347e49cc4fd374f7efda7abfbd107ac4
Walid Jamarin <walidjamarin21@gmail.com>
2020-12-21 22:11:47 +0800

    add authentication on burger builder app"

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -45,6 +45,7 @@ class ContactData extends Component {
           value: '',
           validation: {
             required: true,
+            isNumeric: true,
             minLength: 5,
             maxLength: 5
           },
@@ -72,7 +73,8 @@ class ContactData extends Component {
           },
           value: '',
           validation: {
-            required: true
+            required: true,
+            isEmail: true
           },
           valid: false,
           touched: false
@@ -102,10 +104,11 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ings,
       price: this.props.price,
-      orderData: formData
+      orderData: formData,
+      userId: this.props.userId
     }
 
-    this.props.onOrderBurger(order);
+    this.props.onOrderBurger(order, this.props.token);
     
   }
 
@@ -126,6 +129,16 @@ class ContactData extends Component {
 
     if(rules.maxLength) {
       isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if(rules.isEmail) {
+      const pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if(rules.isNumber) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
     }
 
     return isValid;
@@ -191,13 +204,15 @@ const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token,
+    userId: state.auth.userId,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    onOrderBurger: (orderData, token) => dispatch(actions.purchaseBurger(orderData, token))
   };
 };
 
