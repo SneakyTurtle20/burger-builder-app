a34b85453ede4610a5a1fe0a474abf93c07b92a5
Walid Jamarin <walidjamarin21@gmail.com>
2021-04-03 23:16:11 +0800

    fix auth saga error when logging in for the first time

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -16,7 +16,7 @@ export function* checkAuthTimeoutSaga(action) {
 
 export function* authUserSaga(action) {
   yield put(actions.authStart());
-
+  
   const authData = {
     email: action.email,
     password: action.password,
@@ -28,14 +28,19 @@ export function* authUserSaga(action) {
   }
   try {
     const response = yield axios.post(url, authData);
-    const expirationDate = yield new Date(new Date().getTime() + response.data.expiresIn * 1000);
-    yield localStorage.setItem('token', response.data.idToken);
-    yield localStorage.setItem('expirationDate', expirationDate);
-    yield localStorage.setItem('userId', response.data.localId);
-    yield put(actions.authSuccess(response.data.idToken, response.data.localId));
-    yield put(action.checkAuthTimeout(response.data.expiresIn));
+
+    const expirationDate = yield new Date(
+      new Date().getTime() + response.data.expiresIn * 1000
+    );
+    yield localStorage.setItem("token", response.data.idToken);
+    yield localStorage.setItem("expirationDate", expirationDate);
+    yield localStorage.setItem("userId", response.data.localId);
+    yield put(
+      actions.authSuccess(response.data.idToken, response.data.localId)
+    );
+    yield put(actions.checkAuthTimeout(response.data.expiresIn));
   } catch (error) {
-    yield put (actions.authFail(error.response.data.error.message));
+    yield put(actions.authFail(error.response.data.error));
   }
 }
 
